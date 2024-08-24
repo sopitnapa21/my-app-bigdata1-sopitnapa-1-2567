b6642712a23fc5ca0525fbf225f062c5cbed67f6
“natthaporn6540301816” <natthaporn.we@ku.th>
2024-08-24 12:56:17 +0700

    first commit

---
+++ src/app/editProduct/[id]/page.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Main from "../../components/Main";
+
+export default function page() {
+  return (
+    <div>
+      <Main />
+    </div>
+  );
+}
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -29,7 +29,7 @@ export default function Navbar() {
                 <a href="/chat">Chat GPT</a>
               </li>
               <li className="text-stone-600 hover:text-blue-600 hover:font-bold font-medium mx-4 my-1">
-                <a href="/evaluation">การประเมินนโยบายสาธารณะ</a>
+                <a href="/productlist">การประเมินนโยบายสาธารณะ</a>
               </li>
               <li className="text-stone-600 hover:text-blue-600 hover:font-bold font-medium mx-4 my-1">
                 <a href="#">ทีมงาน</a>
